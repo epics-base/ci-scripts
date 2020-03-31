ee803fc38dad4819bd11c20c792a8ee4b51aee24
Ralph Lange <ralph.lange@gmx.de>
2020-03-31 10:59:31 +0200

    example: use TYPED_RSET for xxxRecord.c

--- exampleApp/src/xxxRecord.c
+++ exampleApp/src/xxxRecord.c
@@ -1,6 +1,6 @@
 /* xxxRecord.c */
 /* Example record support module */
-  
+
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -25,111 +25,111 @@
 /* Create RSET - Record Support Entry Table */
 #define report NULL
 #define initialize NULL
-static long init_record();
-static long process();
+static long init_record(struct dbCommon *, int);
+static long process(struct dbCommon *);
 #define special NULL
 #define get_value NULL
 #define cvt_dbaddr NULL
 #define get_array_info NULL
 #define put_array_info NULL
-static long get_units();
-static long get_precision();
+static long get_units(DBADDR *, char *);
+static long get_precision(const DBADDR *, long *);
 #define get_enum_str NULL
 #define get_enum_strs NULL
 #define put_enum_str NULL
-static long get_graphic_double();
-static long get_control_double();
-static long get_alarm_double();
- 
+static long get_graphic_double(DBADDR *, struct dbr_grDouble *);
+static long get_control_double(DBADDR *, struct dbr_ctrlDouble *);
+static long get_alarm_double(DBADDR *, struct dbr_alDouble *);
+
 rset xxxRSET={
-	RSETNUMBER,
-	report,
-	initialize,
-	init_record,
-	process,
-	special,
-	get_value,
-	cvt_dbaddr,
-	get_array_info,
-	put_array_info,
-	get_units,
-	get_precision,
-	get_enum_str,
-	get_enum_strs,
-	put_enum_str,
-	get_graphic_double,
-	get_control_double,
-	get_alarm_double
+    RSETNUMBER,
+    report,
+    initialize,
+    init_record,
+    process,
+    special,
+    get_value,
+    cvt_dbaddr,
+    get_array_info,
+    put_array_info,
+    get_units,
+    get_precision,
+    get_enum_str,
+    get_enum_strs,
+    put_enum_str,
+    get_graphic_double,
+    get_control_double,
+    get_alarm_double
 };
 epicsExportAddress(rset,xxxRSET);
 
 typedef struct xxxset { /* xxx input dset */
-	long		number;
-	DEVSUPFUN	dev_report;
-	DEVSUPFUN	init;
-	DEVSUPFUN	init_record; /*returns: (-1,0)=>(failure,success)*/
-	DEVSUPFUN	get_ioint_info;
-	DEVSUPFUN	read_xxx;
+    long		number;
+    DEVSUPFUN	dev_report;
+    DEVSUPFUN	init;
+    DEVSUPFUN	init_record; /*returns: (-1,0)=>(failure,success)*/
+    DEVSUPFUN	get_ioint_info;
+    DEVSUPFUN	read_xxx;
 }xxxdset;
 
 static void checkAlarms(xxxRecord *prec);
 static void monitor(xxxRecord *prec);
-
-static long init_record(void *precord,int pass)
+
+static long init_record(struct dbCommon *pcommon, int pass)
 {
-    xxxRecord	*prec = (xxxRecord *)precord;
+    xxxRecord *prec = (xxxRecord *)pcommon;
     xxxdset	*pdset;
     long	status;
 
     if (pass==0) return(0);
 
     if(!(pdset = (xxxdset *)(prec->dset))) {
-	recGblRecordError(S_dev_noDSET,(void *)prec,"xxx: init_record");
-	return(S_dev_noDSET);
+    recGblRecordError(S_dev_noDSET,(void *)prec,"xxx: init_record");
+    return(S_dev_noDSET);
     }
     /* must have read_xxx function defined */
     if( (pdset->number < 5) || (pdset->read_xxx == NULL) ) {
-	recGblRecordError(S_dev_missingSup,(void *)prec,"xxx: init_record");
-	return(S_dev_missingSup);
+    recGblRecordError(S_dev_missingSup,(void *)prec,"xxx: init_record");
+    return(S_dev_missingSup);
     }
 
     if( pdset->init_record ) {
-	if((status=(*pdset->init_record)(prec))) return(status);
+    if((status=(*pdset->init_record)(prec))) return(status);
     }
     return(0);
 }
-
-static long process(void *precord)
+
+static long process(struct dbCommon *pcommon)
 {
-	xxxRecord	*prec = (xxxRecord *)precord;
-	xxxdset		*pdset = (xxxdset *)(prec->dset);
-	long		 status;
-	unsigned char    pact=prec->pact;
+    xxxRecord	*prec = (xxxRecord *)pcommon;
+    xxxdset		*pdset = (xxxdset *)(prec->dset);
+    long		 status;
+    unsigned char    pact=prec->pact;
 
-	if( (pdset==NULL) || (pdset->read_xxx==NULL) ) {
-		prec->pact=TRUE;
-		recGblRecordError(S_dev_missingSup,(void *)prec,"read_xxx");
-		return(S_dev_missingSup);
-	}
+    if( (pdset==NULL) || (pdset->read_xxx==NULL) ) {
+        prec->pact=TRUE;
+        recGblRecordError(S_dev_missingSup,(void *)prec,"read_xxx");
+        return(S_dev_missingSup);
+    }
 
-	/* pact must not be set until after calling device support */
-	status=(*pdset->read_xxx)(prec);
-	/* check if device support set pact */
-	if ( !pact && prec->pact ) return(0);
-	prec->pact = TRUE;
+    /* pact must not be set until after calling device support */
+    status=(*pdset->read_xxx)(prec);
+    /* check if device support set pact */
+    if ( !pact && prec->pact ) return(0);
+    prec->pact = TRUE;
 
-	recGblGetTimeStamp(prec);
-	/* check for alarms */
-	checkAlarms(prec);
-	/* check event list */
-	monitor(prec);
-	/* process the forward scan link record */
+    recGblGetTimeStamp(prec);
+    /* check for alarms */
+    checkAlarms(prec);
+    /* check event list */
+    monitor(prec);
+    /* process the forward scan link record */
         recGblFwdLink(prec);
 
-	prec->pact=FALSE;
-	return(status);
+    prec->pact=FALSE;
+    return(status);
 }
-
+
 static long get_units(DBADDR *paddr, char *units)
 {
     xxxRecord	*prec=(xxxRecord *)paddr->precord;
@@ -138,7 +138,7 @@ static long get_units(DBADDR *paddr, char *units)
     return(0);
 }
 
-static long get_precision(DBADDR *paddr, long *precision)
+static long get_precision(const DBADDR *paddr, long *precision)
 {
     xxxRecord	*prec=(xxxRecord *)paddr->precord;
 
@@ -176,8 +176,8 @@ static long get_control_double(DBADDR *paddr,struct dbr_ctrlDouble *pcd)
     || fieldIndex == xxxRecordHIGH
     || fieldIndex == xxxRecordLOW
     || fieldIndex == xxxRecordLOLO) {
-	pcd->upper_ctrl_limit = prec->hopr;
-	pcd->lower_ctrl_limit = prec->lopr;
+    pcd->upper_ctrl_limit = prec->hopr;
+    pcd->lower_ctrl_limit = prec->lopr;
     } else recGblGetControlDouble(paddr,pcd);
     return(0);
 }
@@ -195,79 +195,79 @@ static long get_alarm_double(DBADDR *paddr,struct dbr_alDouble *pad)
     } else recGblGetAlarmDouble(paddr,pad);
     return(0);
 }
-
+
 static void checkAlarms(xxxRecord *prec)
 {
     double		val, hyst, lalm;
     float		hihi, high, low, lolo;
-	unsigned short	hhsv, llsv, hsv, lsv;
+    unsigned short	hhsv, llsv, hsv, lsv;
 
-	if(prec->udf == TRUE ){
+    if(prec->udf == TRUE ){
         recGblSetSevr(prec,UDF_ALARM,INVALID_ALARM);
-		return;
-	}
-	hihi = prec->hihi; lolo = prec->lolo; high = prec->high; low = prec->low;
-	hhsv = prec->hhsv; llsv = prec->llsv; hsv = prec->hsv; lsv = prec->lsv;
-	val = prec->val; hyst = prec->hyst; lalm = prec->lalm;
+        return;
+    }
+    hihi = prec->hihi; lolo = prec->lolo; high = prec->high; low = prec->low;
+    hhsv = prec->hhsv; llsv = prec->llsv; hsv = prec->hsv; lsv = prec->lsv;
+    val = prec->val; hyst = prec->hyst; lalm = prec->lalm;
 
-	/* alarm condition hihi */
-	if (hhsv && (val >= hihi || ((lalm==hihi) && (val >= hihi-hyst)))){
-	        if (recGblSetSevr(prec,HIHI_ALARM,prec->hhsv)) prec->lalm = hihi;
-		return;
-	}
+    /* alarm condition hihi */
+    if (hhsv && (val >= hihi || ((lalm==hihi) && (val >= hihi-hyst)))){
+            if (recGblSetSevr(prec,HIHI_ALARM,prec->hhsv)) prec->lalm = hihi;
+        return;
+    }
 
-	/* alarm condition lolo */
-	if (llsv && (val <= lolo || ((lalm==lolo) && (val <= lolo+hyst)))){
-	        if (recGblSetSevr(prec,LOLO_ALARM,prec->llsv)) prec->lalm = lolo;
-		return;
-	}
+    /* alarm condition lolo */
+    if (llsv && (val <= lolo || ((lalm==lolo) && (val <= lolo+hyst)))){
+            if (recGblSetSevr(prec,LOLO_ALARM,prec->llsv)) prec->lalm = lolo;
+        return;
+    }
 
-	/* alarm condition high */
-	if (hsv && (val >= high || ((lalm==high) && (val >= high-hyst)))){
-	        if (recGblSetSevr(prec,HIGH_ALARM,prec->hsv)) prec->lalm = high;
-		return;
-	}
+    /* alarm condition high */
+    if (hsv && (val >= high || ((lalm==high) && (val >= high-hyst)))){
+            if (recGblSetSevr(prec,HIGH_ALARM,prec->hsv)) prec->lalm = high;
+        return;
+    }
 
-	/* alarm condition low */
-	if (lsv && (val <= low || ((lalm==low) && (val <= low+hyst)))){
-	        if (recGblSetSevr(prec,LOW_ALARM,prec->lsv)) prec->lalm = low;
-		return;
-	}
+    /* alarm condition low */
+    if (lsv && (val <= low || ((lalm==low) && (val <= low+hyst)))){
+            if (recGblSetSevr(prec,LOW_ALARM,prec->lsv)) prec->lalm = low;
+        return;
+    }
 
-	/* we get here only if val is out of alarm by at least hyst */
-	prec->lalm = val;
-	return;
+    /* we get here only if val is out of alarm by at least hyst */
+    prec->lalm = val;
+    return;
 }
-
+
 static void monitor(xxxRecord *prec)
 {
-	unsigned short	monitor_mask;
-	double		delta;
+    unsigned short	monitor_mask;
+    double		delta;
 
         monitor_mask = recGblResetAlarms(prec);
-	/* check for value change */
-	delta = prec->mlst - prec->val;
-	if(delta<0.0) delta = -delta;
-	if (delta > prec->mdel) {
-		/* post events for value change */
-		monitor_mask |= DBE_VALUE;
-		/* update last value monitored */
-		prec->mlst = prec->val;
-	}
+    /* check for value change */
+    delta = prec->mlst - prec->val;
+    if(delta<0.0) delta = -delta;
+    if (delta > prec->mdel) {
+        /* post events for value change */
+        monitor_mask |= DBE_VALUE;
+        /* update last value monitored */
+        prec->mlst = prec->val;
+    }
 
-	/* check for archive change */
-	delta = prec->alst - prec->val;
-	if(delta<0.0) delta = -delta;
-	if (delta > prec->adel) {
-		/* post events on value field for archive change */
-		monitor_mask |= DBE_LOG;
-		/* update last archive value monitored */
-		prec->alst = prec->val;
-	}
+    /* check for archive change */
+    delta = prec->alst - prec->val;
+    if(delta<0.0) delta = -delta;
+    if (delta > prec->adel) {
+        /* post events on value field for archive change */
+        monitor_mask |= DBE_LOG;
+        /* update last archive value monitored */
+        prec->alst = prec->val;
+    }
 
-	/* send out monitors connected to the value field */
-	if (monitor_mask){
-		db_post_events(prec,&prec->val,monitor_mask);
-	}
-	return;
+    /* send out monitors connected to the value field */
+    if (monitor_mask){
+        db_post_events(prec,&prec->val,monitor_mask);
+    }
+    return;
 }
