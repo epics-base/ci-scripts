epics-base/ci-scripts

commit 35f5befa9d06caea5e882410e57b9e3311aaf279

cue: fix misleading error message (setup file not found)

cue.py
@@ -254,9 +254,9 @@ def source_set(name):
                         setup[assign[0]] = assign[1]
             logger.debug('Done with setup file %s', set_file)
             break
-    else:
-        raise NameError("{0}Setup file {1} does not exist in SETUP_PATH search path ({2}){3}"
-                        .format(ANSI_RED, name, setup_dirs, ANSI_RESET))
+        else:
+            raise NameError("{0}Setup file {1}.set does not exist in SETUP_PATH search path ({2}){3}"
+                            .format(ANSI_RED, name, setup_dirs, ANSI_RESET))
 
 
 # update_release_local(var, location)

commit 12d769835e1ee88e94b5b0b4bc0cf7068e678064

cue: fix issue introduced in 35f5befa
- was leading to jobs failing if setup file was
  not found in the first directory of search path

cue.py
@@ -271,9 +271,9 @@ def source_set(name):
                         setup[assign[0]] = assign[1]
             logger.debug('Done with setup file %s', set_file)
             break
-        else:
-            raise NameError("{0}Setup file {1}.set does not exist in SETUP_PATH search path ({2}){3}"
-                            .format(ANSI_RED, name, setup_dirs, ANSI_RESET))
+    else:
+        raise NameError("{0}Setup file {1}.set does not exist in SETUP_PATH search path ({2}){3}"
+                        .format(ANSI_RED, name, setup_dirs, ANSI_RESET))
 
 
 # update_release_local(var, location)