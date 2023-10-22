899b18336b4ce3bd9328fd30c33621224c78a4d7
Michael Davidsaver <mdavidsaver@gmail.com>
2023-10-22 13:26:08 -0700

    workaround make -Otarget bug on windows
    Sometimes triggers "invalid output sync mutex" error from make.

--- cue.py
+++ cue.py
@@ -452,7 +452,9 @@ def call_make(args=None, **kws):
         makeargs = []
     else:
         makeargs = ['-j{0}'.format(parallel)]
-        if not is_make3:
+        if not is_make3 and ci['os'] != 'windows':
+            # not available until make 3
+            # buggy on windows https://github.com/epics-base/ci-scripts/issues/84
             makeargs += ['-Otarget']
     if silent:
         makeargs += ['-s']
